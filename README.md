6a0ce1ce72d305afa274aad9e24fea06708927c9
Ray Luna <leon.luna.ray@gmail.com>
2023-05-14 01:00:15 -0700

    install vite, upgrade to react 18

---
+++ README.md
@@ -0,0 +1,13 @@
+# Lucky Yasss
+Refresh if you're lucky!
+
+- [https://lucky-yasss.netlify.app/](https://lucky-yasss.netlify.app/)
+
+## Technologies
+
+- React.js
+- Vite.js
+- JavaScript
+- HTML5
+- CSS3
+- Netlify
--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,10 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+
 function getNum() {
     return Math.floor(Math.random() * 10) + 1;
 }
@@ -11,7 +18,7 @@ class NumPicker extends React.Component {
                 <p>{num === 7 ? 'Congrats!' : 'Unlucky!'}</p>
                 {
                     num === 7
-                    ? <img src="https://tenor.com/view/rihanna-work-yaas-dance-dress-gif-23368297" />
+                    ? <img src="https://media1.tenor.com/images/f3321d0fc2b66c005cae93f9f0846b7c/tenor.gif?itemid=5498264" />
                     : null}
             </div>
 
@@ -19,4 +26,4 @@ class NumPicker extends React.Component {
     }
 }
 
-ReactDOM.render(<NumPicker />, document.getElementById('app'));
+root.render(<NumPicker />, document.getElementById('app'));
